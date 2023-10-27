381e1d9de47f55fdde28e4343fd0520cafed786d
Issac Lee <ill8266@rit.edu>
2023-10-27 18:38:58 -0400

    Fixes #30

--- CLI/src/main/java/com/CSCI32006/CLI/CliApplication.java
+++ CLI/src/main/java/com/CSCI32006/CLI/CliApplication.java
@@ -1,8 +1,11 @@
 package com.CSCI32006.CLI;
 
+import org.jline.utils.ShutdownHooks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.CommandScan;
 
+@CommandScan
 @SpringBootApplication
 public class CliApplication {
 
